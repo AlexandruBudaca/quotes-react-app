6355152894f1d8c88f58a0100d2bb30979ae4f80
Alex <alex.andrul10@gmail.com>
2020-06-24 19:42:00 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [err, setErr] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  function fetchQuotesApi() {
+    setLoaded(true);
+    fetch("https://cyf-alexandrubudaca-quotes-api.herokuapp.com/quotes/random")
+      .then((res) => res.json())
+      .then((data) => setQuotes(data))
+      .catch((err) => setErr(err));
+    setLoaded(false);
+  }
+  useEffect(() => {
+    fetchQuotesApi();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {err && (
+          <div>
+            <div>404</div>
+            <div>Sorry, something went wrong!</div>
+          </div>
+        )}
+        {loaded ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : err ? null : (
+          <div className="quotes-card">
+            <div>
+              <h2>{quotes.quote}</h2>
+            </div>
+            <div>
+              <p>{quotes.author}</p>
+            </div>
+            <button onClick={fetchQuotesApi}>New Quote</button>
+          </div>
+        )}
       </header>
     </div>
   );
